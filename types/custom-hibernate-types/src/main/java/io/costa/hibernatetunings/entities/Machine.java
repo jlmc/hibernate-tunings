bbbbb219dabed615c7e5648a8559f4e7c3e84855
Joao Costa <costajlmpp@gmail.com>
2018-10-26 00:17:08 +0100

    hibernate Ipv4 custom type

--- types/custom-hibernate-types/src/main/java/io/costa/hibernatetunings/entities/Machine.java
+++ types/custom-hibernate-types/src/main/java/io/costa/hibernatetunings/entities/Machine.java
@@ -1,14 +1,22 @@
 package io.costa.hibernatetunings.entities;
 
+import io.costa.hibernatetunings.customtype.IPv4;
+import io.costa.hibernatetunings.customtype.IPv4Type;
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "machine")
+@TypeDef(name = "ipv4", defaultForType = IPv4.class, typeClass = IPv4Type.class)
 public class Machine {
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(columnDefinition = "smallint", nullable = false)
+    Type type = Type.PC;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,28 +27,63 @@ public class Machine {
     @Column(name = "mac_address", nullable = false, unique = true, length = 48)
     private String macAddress;
 
-    @Enumerated(EnumType.ORDINAL)
-    @Column(columnDefinition = "smallint", nullable = false)
-    Type type = Type.PC;
-
     @Column(name = "buy_day", nullable = false)
     private LocalDate buyDay;
 
-    @Column(name = "start")
-    private LocalTime start;
+    //@Column(name = "start", columnDefinition = "time")
+    // private LocalTime start;
+
+    @Column(name = "last_know_ip", columnDefinition = "inet")
+    private IPv4 lastKnowIp;
 
     public Machine() {
     }
 
-    private Machine(final String macAddress, final Type type, final LocalDate buyDay, final LocalTime start) {
+    private Machine(final String macAddress, final Type type, final LocalDate buyDay) {
         this.macAddress = macAddress;
         this.type = type;
         this.buyDay = buyDay;
-        this.start = start;
     }
 
-    public static Machine of(final String macAddress, final Type type, final LocalDate buyDay, final LocalTime start) {
-        return new Machine(macAddress, type, buyDay, start);
+    public static Machine of(final String macAddress, final Type type, final LocalDate buyDay) {
+        return new Machine(macAddress, type, buyDay);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Machine machine = (Machine) o;
+        return Objects.nonNull(this.id) && Objects.equals(id, machine.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public LocalDate getBuyDay() {
+        return buyDay;
+    }
+
+    public IPv4 getLastKnowIp() {
+        return lastKnowIp;
+    }
+
+    public void setLastKnowIp(final IPv4 lastKnowIp) {
+        this.lastKnowIp = lastKnowIp;
     }
 
     public enum Type {
--- types/custom-hibernate-types/src/test/java/io/costax/hibernatetunning/customtypes/CustomTypes.java
+++ types/custom-hibernate-types/src/test/java/io/costax/hibernatetunning/customtypes/CustomTypes.java
@@ -1,12 +1,12 @@
 package io.costax.hibernatetunning.customtypes;
 
+import io.costa.hibernatetunings.customtype.IPv4;
 import io.costa.hibernatetunings.entities.Machine;
 import io.costax.rules.EntityManagerProvider;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.Month;
 
 public class CustomTypes {
@@ -18,11 +18,20 @@ public class CustomTypes {
     public void test() {
         provider.beginTransaction();
 
-        Machine machine = Machine.of("ABC", Machine.Type.MAC, LocalDate.of(2017, Month.JANUARY, 3), LocalTime.of(14, 45));
+        Machine machine = Machine.of("ABC", Machine.Type.MAC, LocalDate.of(2017, Month.JANUARY, 3));
 
         provider.em().persist(machine);
 
         provider.commitTransaction();
+    }
 
+    @Test
+    public void updateLastIPAddress() {
+        provider.beginTransaction();
+
+        Machine machine = provider.em().find(Machine.class, 1L);
+        machine.setLastKnowIp(IPv4.of("192.168.3.34"));
+
+        provider.commitTransaction();
     }
 }
