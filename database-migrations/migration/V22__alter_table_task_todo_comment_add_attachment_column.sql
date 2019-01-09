f356747b9efc3879f2ad3482658a97ed4ee3e56f
Joao Costa <costajlmpp@gmail.com>
2019-01-09 01:23:37 +0000

    Paginatio nOne To Many With Lazy Field Test

---
+++ database-migrations/migration/V22__alter_table_task_todo_comment_add_attachment_column.sql
@@ -0,0 +1,2 @@
+alter table tasks.todo_comment
+	add attachment bytea;
---
+++ hibernate-tuning-howtos/query-pagination-using-window-functions/src/main/java/io/costax/hibernatetunning/tasks/Todo.java
@@ -0,0 +1,106 @@
+package io.costax.hibernatetunning.tasks;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+@Table(schema = "tasks", name = "todo")
+@NamedNativeQuery(
+        name = "TodoWithCommentByRank",
+        query =
+                "SELECT * " +
+                        "FROM (   " +
+                        "    SELECT *, dense_rank() OVER (ORDER BY \"td.title\", \"tdc.todo_id\") rank " +
+                        "    FROM (   " +
+                        "        SELECT td.id AS \"td.id\", " +
+                        "               td.title AS \"td.title\", " +
+                        // "               td.title AS \"td.title\", " +
+                        "               tdc.id as \"tdc.id\", " +
+                        "               tdc.review AS \"tdc.review\", " +
+                        // "               td.review AS \"tdc.review\", " +
+                        "               tdc.todo_id AS \"tdc.todo_id\" " +
+                        //"              , tdc.attachment AS \"tdc.attachment\" " +
+                        "        FROM tasks.todo td  " +
+                        "        LEFT JOIN tasks.todo_comment tdc ON td.id = tdc.todo_id  " +
+                        "        ORDER BY td.id " +
+                        "    ) p_pc " +
+                        ") p_pc_r " +
+                        "WHERE p_pc_r.rank between :firstRecord and :lastRecord",
+        resultSetMapping = "TodoWithCommentByRankMapping"
+)
+@SqlResultSetMapping(
+        name = "TodoWithCommentByRankMapping",
+        entities = {
+                @EntityResult(
+                        entityClass = Todo.class,
+                        fields = {
+                                @FieldResult(name = "id", column = "td.id"),
+                                @FieldResult(name = "title", column = "td.title"),
+                        }
+                ),
+                @EntityResult(
+                        entityClass = TodoComment.class,
+                        fields = {
+                                @FieldResult(name = "id", column = "tdc.id"),
+                                @FieldResult(name = "review", column = "tdc.review"),
+                                @FieldResult(name = "todo", column = "tdc.todo_id"),
+                        }
+                )
+        }
+)
+public class Todo {
+
+    @Id
+    private Long id;
+    private String title;
+
+    @OneToMany(
+            mappedBy = "todo",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<TodoComment> comments = new ArrayList<>();
+
+    protected Todo() {
+    }
+
+    private Todo(final Long id, final String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public static Todo of(final Long id, final String title) {
+        return new Todo(id, title);
+    }
+
+    public void addComment(TodoComment comment) {
+        comments.add(comment);
+        comment.setTodo(this);
+    }
+
+    public void removeComment(TodoComment comment) {
+        comments.remove(comment);
+        comment.setTodo(null);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<TodoComment> getComments() {
+        return Collections.unmodifiableList(comments);
+    }
+
+    public void setComments(final ArrayList<TodoComment> objects) {
+        this.comments = objects;
+    }
+
+
+}
+
