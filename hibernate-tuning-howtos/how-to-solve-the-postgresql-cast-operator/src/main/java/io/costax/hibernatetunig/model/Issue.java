b7b69128369184f468861539d676ac88d9bf5c66
Joao Costa <costajlmpp@gmail.com>
2019-01-12 16:37:36 +0000

    how to solve cast in postgresSql

---
+++ hibernate-tuning-howtos/how-to-solve-the-postgresql-cast-operator/src/main/java/io/costax/hibernatetunig/model/Issue.java
@@ -0,0 +1,72 @@
+package io.costax.hibernatetunig.model;
+
+import io.costax.hibernatetunig.model.projections.IssueSummary;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "issue")
+@SqlResultSetMapping(
+        name = "IssueSummaryMapper",
+        classes = @ConstructorResult(
+                targetClass = IssueSummary.class,
+                columns = {
+                        @ColumnResult(name = "id", type = Long.class),
+                        @ColumnResult(name = "title", type = String.class),
+                        @ColumnResult(name = "day", type = LocalDate.class),
+                }
+        )
+)
+public class Issue extends BaseEntity {
+
+    @ManyToOne //(cascade = {PERSIST, MERGE, REMOVE})
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+
+    private String title;
+    private String description;
+
+    //@CreationTimestamp
+    @Column(name = "create_at")
+    private OffsetDateTime createAt;
+
+    public Issue() {
+    }
+
+    private Issue(final Project project, final String title, final OffsetDateTime createAt) {
+        this.project = project;
+        this.title = title;
+        this.createAt = createAt;
+    }
+
+    public static Issue of(final Project project, final String title, final OffsetDateTime createAt) {
+        return new Issue(project, title, createAt);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Issue issue = (Issue) o;
+        return getId() != null && Objects.equals(createAt, issue.createAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    public void changeProject(final Project project, final String title) {
+        this.project = project;
+        this.title = title;
+
+    }
+
+    protected void setProject(final Project project) {
+        this.project = project;
+    }
+}
