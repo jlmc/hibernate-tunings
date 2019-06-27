39c610f1cb32f83d9a2a69fa99e2168f43336a78
Joao Costa <costajlmpp@gmail.com>
2019-06-27 23:30:25 +0100

    Relationships Many To One

---
+++ relationships/src/main/java/io/costax/relationships/Director.java
@@ -0,0 +1,41 @@
+package io.costax.relationships;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Director {
+
+    @Id private Integer id;
+    private String name;
+
+    //@OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    //List<Article> articles = new ArrayList<>();
+
+    protected Director() {
+    }
+
+    private Director(final Integer id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Director of(final Integer id, final String name) {
+        return new Director(id, name);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Director)) return false;
+        final Director author = (Director) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
