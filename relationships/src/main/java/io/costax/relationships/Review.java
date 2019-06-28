c7fa1ec4b7e9ebe40f27e05e066443b0dc34ea8e
Joao Costa <costajlmpp@gmail.com>
2019-06-28 01:24:03 +0100

    Relationships OneToMany Bidirectional

---
+++ relationships/src/main/java/io/costax/relationships/Review.java
@@ -0,0 +1,47 @@
+package io.costax.relationships;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "movie_id",
+            updatable = false,
+            nullable = false
+    )
+    private Movie movie;
+
+    protected Review() {}
+
+    public static Review of (final String comment) {
+        Review review = new Review();
+        review.comment = comment;
+        return review;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Review)) return false;
+        final Review review = (Review) o;
+        return this.id != null && Objects.equals(id, review.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    void setMovie(final Movie movie) {
+        this.movie = movie;
+    }
+}
