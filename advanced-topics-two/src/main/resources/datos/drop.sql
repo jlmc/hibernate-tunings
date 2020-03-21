bd834409efcae013b3880d107d1aab3865c76221
Joao Costa <costajlmpp@gmail.com>
2020-03-21 02:18:49 +0000

    How to Create An Entity Graph With Multiple Sub Graphs fix drop schema
    - add missing table

--- advanced-topics-two/src/main/resources/datos/drop.sql
+++ advanced-topics-two/src/main/resources/datos/drop.sql
@@ -1,4 +1,10 @@
-drop table if exists Machine;
-drop table if exists Developer;
-drop table if exists Vehicle;
-drop table if exists Garage;
+drop table if exists Machine cascade;
+drop table if exists Developer cascade;
+drop table if exists Vehicle cascade;
+drop table if exists Garage cascade;
+drop table if exists Employee cascade;
+drop table if exists book_author cascade;
+drop table if exists Author cascade;
+drop table if exists Book cascade;
+drop table if exists Publisher cascade;
+
