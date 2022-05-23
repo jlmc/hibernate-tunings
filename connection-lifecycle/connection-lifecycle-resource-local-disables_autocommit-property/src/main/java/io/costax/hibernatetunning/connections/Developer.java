22a88333afeb2a7fde2844acf0bd0775aa35666e
Joao Morgado Costa <joao.morgado@talkdesk.com>
2022-05-23 19:51:56 +0100

    fixing connection-lifecycle-resource-local-disables_autocommit-property project

--- connection-lifecycle/connection-lifecycle-resource-local-disables_autocommit-property/src/main/java/io/costax/hibernatetunning/connections/Developer.java
+++ connection-lifecycle/connection-lifecycle-resource-local-disables_autocommit-property/src/main/java/io/costax/hibernatetunning/connections/Developer.java
@@ -1,8 +1,18 @@
 package io.costax.hibernatetunning.connections;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.*;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
