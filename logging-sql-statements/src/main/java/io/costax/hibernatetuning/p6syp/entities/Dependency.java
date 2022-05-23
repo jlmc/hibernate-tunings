8d46dfcb482322d63dc6ca305d879285cd7b669d
Joao Morgado Costa <joao.morgado@talkdesk.com>
2022-05-23 19:46:27 +0100

    fixing logging-sql-statements project

--- logging-sql-statements/src/main/java/io/costax/hibernatetuning/p6syp/entities/Dependency.java
+++ logging-sql-statements/src/main/java/io/costax/hibernatetuning/p6syp/entities/Dependency.java
@@ -1,11 +1,12 @@
 package io.costax.hibernatetuning.p6syp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @Entity
+@Table(name = "t_dependency")
 public class Dependency {
 
     @Id
