0c2813f2aa8a32084a68faefbd2ca9b49c8dfdff
Joao Morgado Costa <joao.morgado@talkdesk.com>
2022-05-29 23:25:48 +0100

    fixing bulk-operations project

--- bulk-operations/src/main/java/io/costax/hibernatetunings/entities/project/Issue.java
+++ bulk-operations/src/main/java/io/costax/hibernatetunings/entities/project/Issue.java
@@ -1,8 +1,12 @@
 package io.costax.hibernatetunings.entities.project;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
